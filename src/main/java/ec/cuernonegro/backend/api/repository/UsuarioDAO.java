e9d44d6c89f6b10e2d743e84c6e24047655e3dfe
Erick-Nu <erick.nunez02@epn.edu.ec>
2026-02-17 23:15:22 -0500

    [Feature]: Crear el contrato de las operaciones o métodos de la entidad usuario para despues realizar la implementación de cada uno

---
+++ src/main/java/ec/cuernonegro/backend/api/repository/UsuarioDAO.java
@@ -0,0 +1,13 @@
+package ec.cuernonegro.backend.api.repository;
+import ec.cuernonegro.backend.api.models.Usuario;
+import java.sql.SQLException;
+
+public interface UsuarioDAO {
+    // Here we will define the methods we can use with our users.
+    boolean saveUser(Usuario user) throws SQLException;
+    Usuario findUserByEmail(String email) throws SQLException;
+    Usuario findUserById(int userid) throws SQLException;
+    boolean updateUser (Usuario user) throws SQLException;
+    String updateUserToken (int userid, String usertoken) throws SQLException;
+    boolean existUserWithEmail(String email) throws SQLException;
+}
